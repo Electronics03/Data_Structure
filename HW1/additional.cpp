b3d8059a1554dc2630491aea7c2ba4b6b3db2158
Electronics03 <funfunpsh@gmail.com>
2025-04-08 12:52:29 +0900

    추가 과제 완료료

---
+++ HW1/additional.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include "CDList.h"
+
+using namespace std;
+
+bool decode(string &opCode, CDList &playList)
+{
+	if (opCode == "add")
+	{
+		string input;
+		cin >> input;
+		std::cout << "add(" << input << ")" << std::endl;
+		playList.add(input);
+		std::cout << playList << std::endl;
+		return true;
+	}
+	else if (opCode == "remove")
+	{
+		std::cout << "remove()" << std::endl;
+		playList.remove();
+		std::cout << playList << std::endl;
+		return true;
+	}
+	else if (opCode == "forward")
+	{
+		std::cout << "forward()" << std::endl;
+		playList.forward();
+		std::cout << playList << std::endl;
+		return true;
+	}
+	else if (opCode == "backward")
+	{
+		std::cout << "backward()" << std::endl;
+		playList.backward();
+		std::cout << playList << std::endl;
+		return true;
+	}
+	else if (opCode == "exit")
+	{
+		return false;
+	}
+	else
+	{
+		std::cout << "ERROR: Can not decode." << std::endl;
+		return false;
+	}
+}
+
+int main()
+{
+	CDList playList;
+	bool rp = true;
+	while (rp)
+	{
+		string opCode;
+		cin >> opCode;
+		rp = decode(opCode, playList);
+	}
+	return EXIT_SUCCESS;
+}
+// g++ HW1/additional.cpp HW1/CDList.cpp -o HW1/additional
