4431120421ae2af733b59ee6b429fb5e1b652414
Electronics03 <funfunpsh@gmail.com>
2025-05-11 18:03:36 +0900

    모든 리스트 추가

---
+++ MidtermExam/LinearStructures/CircularDoublyLinkedList.h
@@ -0,0 +1,84 @@
+#ifndef CDLIST_H
+#define CDLIST_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "DataError.h"
+
+using Elem = std::string;
+
+typedef struct Node
+{
+    Elem element;
+    Node *next;
+    Node *prev;
+} Node;
+
+class CDList
+{
+private:
+    Node *cursor;
+
+public:
+    CDList();
+    ~CDList();
+    void add(Elem data);
+    void remove();
+    bool empty();
+    void forward();
+    void backward();
+};
+CDList::CDList() : cursor(nullptr) {}
+CDList::~CDList()
+{
+    while (!empty())
+        remove();
+}
+void CDList::add(Elem data)
+{
+    Node *newNode = new Node;
+    newNode->element = data;
+    if (empty())
+    {
+        cursor = newNode;
+        cursor->next = cursor;
+        cursor->prev = cursor;
+    }
+    else
+    {
+        newNode->next = cursor->next;
+        newNode->prev = cursor;
+        cursor->next = cursor->next->prev = newNode;
+    }
+}
+
+void CDList::remove()
+{
+    if (empty())
+        throw ListError("Error: List is empty.");
+    Node *delNp = cursor->next;
+    if (delNp == cursor)
+    {
+        cursor = nullptr;
+    }
+    else
+    {
+        delNp->next->prev = cursor;
+        cursor->next = delNp->next;
+    }
+    delete delNp;
+}
+bool CDList::empty()
+{
+    return (cursor == nullptr);
+}
+void CDList::forward()
+{
+    cursor = cursor->next;
+}
+void CDList::backward()
+{
+    cursor = cursor->prev;
+}
+#endif
