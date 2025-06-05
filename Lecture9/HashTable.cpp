46e09c4734102517af45ce0826e2e685d661ccad
Electronics03 <funfunpsh@gmail.com>
2025-06-05 10:23:31 +0900

    추가된 코드들

--- Lecture9/HashTable.cpp
+++ Lecture9/HashTable.cpp
@@ -23,23 +23,44 @@ template <typename K, typename V, typename H>
 class HashMap
 {
 public:
-    using EntryType = Entry<K, V>;
-    using Bucket = std::list<EntryType>;
-    using BktArray = std::vector<Bucket>;
-    using Iterator = typename std::list<EntryType>::iterator;
+    using EntryType = Entry<const K, V>;
+    class Iterator;
 
 public:
-    int size(void) const { return data.size(); }
-    bool empty(void) const { return data.empty(); }
-    Iterator find(const K &k) const;
+    HashMap(int capacity = 100);
+    int size(void) const;
+    bool empty(void) const;
+    Iterator find(const K &k);
     Iterator put(const K &k, const V &v);
     void erase(const K &k);
-    Iterator begin(void) const { return data.begin(); }
-    Iterator end(void) const { return data.end(); }
-    void printEnt(void) const;
+    void erase(const Iterator &k);
+    Iterator begin(void);
+    Iterator end(void);
+
+protected:
+    using Bucket = std::list<EntryType>;
+    using BktArray = std::vector<Bucket>;
+    using EItor = Bucket::iterator;
+    using BItor = BktArray::iterator;
 
 private:
     int n;
     H hash;
     BktArray B;
+
+public:
+    class Iterator
+    {
+    private:
+        EItor ent;
+        BItor bkt;
+        const BktArray *ba;
+
+    public:
+        Iterator(const BktArray &a, const BItor &b, const EItor &q = EItor()) : ent(q), bkt(b), ba(&a) {}
+        Entry &operator*() const;
+        bool operator==(const Iterator &p) const;
+        Iterator &operator++(void);
+        friend class HashMap;
+    };
 };
