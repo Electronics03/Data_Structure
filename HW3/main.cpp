923865eab029b83034f4be23368178ff1bf35563
Electronics03 <funfunpsh@gmail.com>
2025-06-14 19:05:16 +0900

    main주석 작성 완료

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -4,13 +4,17 @@
 int main(void)
 {
     HashMap<std::string, int, stringHash> map;
-
+    // key : std::string
+    // value : int
+    // Create a HashMap with string keys and int values
     typedef HashMap<std::string, int, stringHash>::Iterator hashIter;
 
+    // Insert three key-value pairs into the map
     map.put("apple", 100);
     map.put("banana", 150);
     map.put("cherry", 200);
 
+    // Print all entries in the map
     hashIter iter = map.begin();
     while (1)
     {
@@ -21,8 +25,10 @@ int main(void)
     }
     std::cout << std::endl;
 
+    // Erase the entry with key "apple"
     map.erase("apple");
 
+    // Print all entries in the map
     iter = map.begin();
     while (1)
     {
@@ -33,8 +39,10 @@ int main(void)
     }
     std::cout << std::endl;
 
+    // Update the value for key "cherry" to 300
     map.put("cherry", 300);
 
+    // Print all entries in the map
     iter = map.begin();
     while (1)
     {
@@ -45,9 +53,12 @@ int main(void)
     }
     std::cout << std::endl;
 
+    // Find the iterator for "cherry" and erase it
     iter = map.find("cherry");
     map.erase(iter);
     iter = map.begin();
+
+    // Print all entries in the map
     while (1)
     {
         if (iter == map.end())
@@ -57,6 +68,7 @@ int main(void)
     }
     std::cout << std::endl;
 
+    // Erase the remaining entry "banana"
     map.erase("banana");
     if (map.empty())
         std::cout << "map is empty" << std::endl;
